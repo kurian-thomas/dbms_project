d89b4959371c1a480f9c33ccbba7022697cf189c
kurian-thomas <github719@gmail.com>
2018-10-22 18:09:08 +0530

    framework

---
+++ tc_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TcAppConfig(AppConfig):
+    name = 'tc_app'
---
+++ tc_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Signup(models.Model):
+	name=models.CharField(max_length=265)
+	admission_id=models.CharField(max_length=265,unique=True)
+	email=models.EmailField(max_length=70,unique= True)
+	password=models.CharField(max_length=10)
+
+
+
