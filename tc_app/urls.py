8d1b5f23d1a1ba4095bb7611d662ab9ac3b9f4cf
Adarsh Menon <adarsh1021@gmail.com>
2018-10-23 15:03:35 +0530

    tc admin app added

--- tc_app/urls.py
+++ tc_app/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from tc_app import views 
 
+name = 'tc_app'
+
 urlpatterns = [
 	url(r'^index/',views.index,name='index'),
 	url(r'^login/',views.login,name='login'),
 	url(r'^get_element/',views.get_element,name='get_element'),
-
 ]
